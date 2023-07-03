0ca33c2b5ac2299b709667dc26578405fb64cbb0
muhrmps123@gmail.com <muhrmps123@gmail.com>
2023-07-03 16:23:17 +0700

    First Upload

---
+++ kasir/layout_bottom.php
@@ -0,0 +1,23 @@
+	</div><!-- /#wrapper -->
+	
+	<!-- footer-bottom -->
+	<div class="navbar navbar-inverse navbar-fixed-bottom footer-bottom">
+		<div class="container text-center">
+			<p class="text-center" style="color: #D1C4E9; margin: 0 0 5px; padding: 0"><marquee><small>Ciblue Cofee and Toast Menyediakan berbagai macam kopi khas indonesia. Pemesanan dapat melalui Gojek maupun Shopee Food atau bisa menhubungi kami di nomor WA 087838002694 atas nama Ciblue Cofee and Toast. Pemesanan Dibuka Pada hari Kerja dan Hari-hari Libur pada pukul 13.00 Siang s/d Pukul 21.00 malam wib</p></marquee>
+		</div>
+	</div><!-- /.footer-bottom -->
+
+	<!-- Bootstrap Core JavaScript -->
+	<script src="libs/bootstrap/dist/js/bootstrap.min.js"></script>
+	<!-- Metis Menu Plugin JavaScript -->
+	<script src="libs/metisMenu/dist/metisMenu.min.js"></script>
+	<!-- DataTables JavaScript -->
+    <script src="libs/datatables/media/js/jquery.dataTables.min.js"></script>
+    <script src="libs/datatables-plugins/integration/bootstrap/3/dataTables.bootstrap.min.js"></script>
+	<!-- jTebilang JavaScript -->
+	<script src="libs/jTerbilang/jTerbilang.js"></script>
+	<!-- Custom Theme JavaScript -->
+	<script src="dist/js/sb-admin-2.js"></script>
+
+</body>
+</html>
